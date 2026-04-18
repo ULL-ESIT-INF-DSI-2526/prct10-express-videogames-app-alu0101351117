96bef8f25679c8c8801cc23e8cdc3f32ccaafd6b
alu0101351117 <alu0101351117@ull.edu.es>
2026-04-18 10:27:26 +0000

    configuracion inicial

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import tsdoc from "eslint-plugin-tsdoc";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { globals: globals.node } },
+  tseslint.configs.recommended,
+
+  {
+    plugins:
+      {
+        tsdoc
+      }
+  },
+  {
+    rules: {
+      "prefer-const": "off",
+      "tsdoc/syntax": "warn"
+    }
+  },
+  {
+    ignores: [
+      "eslint.config.mjs",
+      "dist/*",
+      "docs/*",
+    ]
+  },
+]);
